12e2ed6c0c463ec609fbbb9ed2ea415ebe3073bf
Emree <emreejensen2002@gmail.com>
2025-03-16 09:57:29 -0400

    Added print method to MyClass - Emree

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,4 +1,9 @@
 public class MyClass {
     //hello everyone!
     int x = 5; //did this work?
+    
+    // New method to print x added by Emree
+    public void printX() {
+        System.out.println("Value of x: " + x);
+    }
 }
