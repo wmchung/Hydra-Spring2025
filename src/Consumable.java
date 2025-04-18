3ff155262ecfb9e804041ca8cf0bcd6d6f0b1d1d
Wesley <wesleymchung@gmail.com>
2025-04-17 22:33:05 -0400

    updates to rooms, items, and its subclasses

--- src/Consumable.java
+++ src/Consumable.java
@@ -1,9 +1,9 @@
 public class Consumable extends Items{
-    private int uses;
+   // private int uses; //
     private int healingPoints;
-    public Consumable(int itemID, String itemName, String itemDescription, String tag, String location, int uses, int healingPoints) {
+    public Consumable(int itemID, String itemName, String itemDescription, String tag, String location, int healingPoints) {
         super(itemID, itemName, itemDescription, tag, location);
-        this.uses=uses;
+       // this.uses=uses;
         this.healingPoints=healingPoints;
     }
 }
