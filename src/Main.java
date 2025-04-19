0a60dcfb3e4872b0f98f1797061c747f2d867e41
Gabbi <gcoroama@ggc.edu>
2025-04-19 18:17:02 -0400

    fixed item.txt

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        GameMap gm = new GameMap();
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            gm.loadGameData("item.txt", "puzzles.txt", "Enemy.txt", "NPC.txt", "Map.txt");
+            System.out.println("All files loaded");
+            break;
+        }
+        input.close();
+    }
+}
+
