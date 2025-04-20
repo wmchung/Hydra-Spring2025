d9c6d56348b9164d826eaa3f2c8931f9e6860869
Emree <emreejensen2002@gmail.com>
2025-04-19 23:40:53 -0400

    Controller & View Updates

--- src/GameController.java
+++ src/GameController.java
@@ -4,18 +4,43 @@ import java.util.Scanner;
 public class GameController {
     private GameMap gameMap;
     private Player player;
-    private Room room;
+    private GameView view;
 
-    public GameController(Player player,Room room) {
-
+    public GameController(GameMap gameMap, Player player) {
         this.gameMap = new GameMap();
         this.player = player;
-        this.room=room;
+        this.view = new GameView();
     }
 
     //method to start game
     public void startGame() {
+        view.showWelcome();
+        boolean playing = true;
 
+        while (playing && player.isAlive()) {
+            view.showPrompt();
+            String command = view.getCommand().toLowerCase();
+
+            switch (command) {
+                case "look":
+                    player.look();
+                    break;
+                case "inventory":
+                    player.showInventory();
+                    break;
+                case "help":
+                    view.showHelp();
+                    break;
+                case "exit":
+                case "quit":
+                    view.showExitMessage();
+                    playing = false;
+                    break;
+                default:
+                    view.showInvalidCommand();
+                    break;
+            }
+        }
     }
     public void help (){
         //final boss room
