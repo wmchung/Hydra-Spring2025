816a360c63c7a9bd566d61ad27805fa6f10985e1
Wesley <wesleymchung@gmail.com>
2025-04-19 22:24:22 -0400

    added methods to player for game logic

--- src/GameController.java
+++ src/GameController.java
@@ -1,2 +1,20 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class GameController {
+    private GameMap gameMap;
+    private Player player;
+
+    public GameController(Player player) {
+        this.gameMap = new GameMap();
+        this.player = player;
+    }
+
+    //method to start game
+    public void startGame() {
+
+    }
+
+
+
 }
